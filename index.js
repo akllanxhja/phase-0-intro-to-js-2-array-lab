fa70f4b132e5b862d58ab58eecdc48d2c2f1405e
AlexanderKllanxhja <akllanxhja@gmail.com>
2021-07-20 16:47:20 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
+// const cats = ['Milo', 'Otis', 'Garfield'];
+
+// function destructivelyAppendCat(name) {
+//     cats.push(name);
+// }
+
+// function destructivelyPrependCat(name) {
+//     cats.unshift(name);
+// }
+//  function destructivelyRemoveLastCat() {
+//      cats.pop();
+//  }
+
+//  function destructivelyRemoveFirstCat() {
+//      cats.shift();
+//  }
+
+//  function appendCat(name) {
+//      return [...cats, name];
+//  }
+//  function prependCat(name) {
+//      return [name, ...cats];
+//  }
+
+//  function removeFirstCat() {
+//      return cats.slice(1);
+//  }
+
+//  function removeLastCat() {
+//      return cats.slice(0, cats.length - 1);
+//  }
+
 const cats = ['Milo', 'Otis', 'Garfield'];
-
 function destructivelyAppendCat(name) {
-    cats.push(name);
+    destructivelyAppendCat = cats.push('Ralph');
 }
-
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
+    destructivelyPrependCat = cats.unshift('Bob');
 }
- function destructivelyRemoveLastCat() {
-     cats.pop();
- }
-
- function destructivelyRemoveFirstCat() {
-     cats.shift();
- }
-
- function appendCat(name) {
-     return [...cats, name];
- }
- function prependCat(name) {
-     return [name, ...cats];
- }
-
- function removeFirstCat() {
-     return cats.slice(1);
- }
-
- function removeLastCat() {
-     return cats.slice(0, cats.length - 1);
- }
+function destructivelyRemoveLastCat(name) {
+    destructivelyRemoveLastCat = cats.pop(-1);
+}
+function destructivelyRemoveFirstCat(name) {
+    destructivelyRemoveFirstCat = cats.shift(0);
+}
+function appendCat(name) {
+    return [...cats, name];
+}
+function prependCat(name) {
+    return [name, ...cats];
+}
+function removeLastCat(name) {
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
